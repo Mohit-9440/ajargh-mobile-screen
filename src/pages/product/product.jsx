9490a5ba33099fc8e9a15aca60dfe4934fdde086
Mohit <mohitsharma8864@gmail.com>
2023-06-04 11:57:51 +0530

    base design

---
+++ src/pages/product/product.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const product = () => {
+  return (
+    <div className='product'>
+        <div></div>
+        <div>
+            <section>
+                
+            </section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </div>
+    </div>
+  )
+}
+
+export default product
