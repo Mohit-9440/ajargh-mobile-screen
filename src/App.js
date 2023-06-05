71ac541bef3a550e8056cf162a764ce56efb0681
Mohit <mohitsharma8864@gmail.com>
2023-06-05 08:39:15 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Home from './pages/home/home';
-// import Product from './pages/product/product';
-// import Payment from './pages/payment/Payment';
+import Product from './pages/product/product';
+import Payment from './pages/payment/Payment';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Product/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/payment" element={<Payment/>} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
